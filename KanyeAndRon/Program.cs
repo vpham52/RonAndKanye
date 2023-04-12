fd160f13e8683a725dd045fda1a8d6753ea5cb1a
Victoria Pham <victoriapham52@gmail.com>
2023-04-11 18:34:58 -0700

    Ron and Ye Exercise

---
+++ KanyeAndRon/Program.cs
@@ -0,0 +1,13 @@
+﻿using KanyeAndRon;
+using Newtonsoft.Json.Linq;
+
+
+
+for (int i = 0; i < 5; i++)
+{
+    Quotes.KanyeQuote();
+    Quotes.RonQuote();
+
+}
+
+
---
+++ KanyeAndRon/Quotes.cs
@@ -0,0 +1,42 @@
+﻿
+using System;
+using Newtonsoft.Json.Linq;
+
+
+namespace KanyeAndRon
+{
+	public class Quotes
+	{
+        public static void KanyeQuote()
+        {
+
+            var client = new HttpClient();
+
+            var kanyeURL = "https://api.kanye.rest/";
+
+            var kanyeResponse = client.GetStringAsync(kanyeURL).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            Console.WriteLine($"Kanye: \"{kanyeQuote}\"\n");
+                      
+        }
+
+        public static void RonQuote()
+        {
+            var client = new HttpClient();
+
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = client.GetStringAsync(ronURL).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            Console.WriteLine($"Ron: {ronQuote}\n");
+
+
+        }
+
+    } 
+}
+
